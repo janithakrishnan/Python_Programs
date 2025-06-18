f00d153657bd9dd514d6654d329ad6d8a6263a6f
Janitha krishnan <janithakrishnan@gmail.com>
2025-06-18 15:30:42 +0530

    Add files via upload

---
+++ LetterCount.py
@@ -0,0 +1,10 @@
+#Letter Count
+alphabet=list('abcdefghijklmnopqrstuvwxyz')
+x=input("ENTER THE STRING:")
+print(alphabet)
+letter_freq={}
+for ltr in x:
+    letter_freq.setdefault(ltr,0)
+    letter_freq[ltr]+=1
+for k,v in sorted(letter_freq.items()):
+    print(f"letter {k} is present {v} times")
